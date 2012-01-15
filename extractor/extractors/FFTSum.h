ee617100a005eb69ed0d68ce585b78f97c349b29
Michał Janiszewski <janisozaur@gmail.com>
2012-01-15 23:52:39 +0100

    output fftsum stats instead of raw values

--- extractor/extractors/FFTSum.h
+++ extractor/extractors/FFTSum.h
@@ -7,24 +7,24 @@
 class FFTSum : public ExtractorInterface
 {
 public:
-    struct QImages {
-        QImage phase, magnitude;
-    };
-    FFTSum();
-    virtual bool init(const QStringList &params);
-    virtual QString name() const;
-    void perform(ComplexArray *ca, bool inverse = false) const;
-    QVector<float> extract(const QImage &image, const int &x, const int &y) const;
-    int size() const;
+	struct QImages {
+		QImage phase, magnitude;
+	};
+	FFTSum();
+	virtual bool init(const QStringList &params);
+	virtual QString name() const;
+	void perform(ComplexArray *ca, bool inverse = false) const;
+	QVector<float> extract(const QImage &image, const int &x, const int &y) const;
+	int size() const;
 //    const QImages complexToImages(const ComplexArray *ca, QImage::Format format) const;
 
 private:
-    void rearrange(QVector<Complex> &elements) const;
-    void transform(QVector<Complex> &elements, bool inverse) const;
-    void oneDFftH(ComplexArray *ca, int idx1, int idx2, bool inverse) const;
-    void oneDFftV(ComplexArray *ca, int idx1, int idx2, bool inverse) const;
+	void rearrange(QVector<Complex> &elements) const;
+	void transform(QVector<Complex> &elements, bool inverse) const;
+	void oneDFftH(ComplexArray *ca, int idx1, int idx2, bool inverse) const;
+	void oneDFftV(ComplexArray *ca, int idx1, int idx2, bool inverse) const;
 
-    QSize mSize;
+	QSize mSize;
 };
 
 #endif // FFTSUM_H
