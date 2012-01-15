5d20db8819a7e8f0a8e5aa976ac3acef5b826801
Michał Janiszewski <janisozaur@gmail.com>
2012-01-15 23:41:32 +0100

    change threshold for train breaking from 1.5 * best error to 30 * best error add some (commented out) debugging code

--- classifier/NeuralNetwork/NeuralNetwork.cpp
+++ classifier/NeuralNetwork/NeuralNetwork.cpp
@@ -10,219 +10,241 @@
 #include <QTextStream>
 #include <limits>
 #ifdef HAS_ELAPSED_TIMER
-    #include <QElapsedTimer>
+	#include <QElapsedTimer>
 #else
-    #include <QTime>
+	#include <QTime>
 #endif
 
 #define TEACH_BIAS_MOMENTUM
 
 NeuralNetwork::NeuralNetwork()
 {
-    id = qrand();
+	id = qrand();
 }
 
 
 QVector<nnreal> NeuralNetwork::classifyVec(const QVector<nnreal> &input)
 {
-    for (int i = 0; i < mLayers.size(); i++) {
-        if (i == 0) {
-            for (int j = 0; j < mLayers.at(0)->neuronsCount(); j++) {
-                mLayers.at(0)->neuron(j)->updateOutput(input[j]);
-            }
-        } else {
-            mLayers.at(i)->calculate();
-        }
-    }
+	for (int i = 0; i < mLayers.size(); i++) {
+		if (i == 0) {
+			for (int j = 0; j < mLayers.at(0)->neuronsCount(); j++) {
+				mLayers.at(0)->neuron(j)->updateOutput(input[j]);
+			}
+		} else {
+			mLayers.at(i)->calculate();
+		}
+	}
 
-    QVector<nnreal> result(mLayers.at(mLayers.size() - 1)->neuronsCount());
-    for (int i = 0; i < result.size(); i++) {
-        result[i] = mLayers.at(mLayers.size() - 1)->neuron(i)->output();
-    }
-    return result;
+	QVector<nnreal> result(mLayers.at(mLayers.size() - 1)->neuronsCount());
+	for (int i = 0; i < result.size(); i++) {
+		result[i] = mLayers.at(mLayers.size() - 1)->neuron(i)->output();
+	}
+	return result;
 }
 
 nnreal NeuralNetwork::backprop(const QVector<nnreal> &input,
-                               const QVector<nnreal> &targetOutput,
-                               const nnreal &learningRate,
-                               const nnreal &momentum)
+							   const QVector<nnreal> &targetOutput,
+							   const nnreal &learningRate,
+							   const nnreal &momentum)
 {
-    const int lastLayerIdx = mLayers.size() - 1;
-    const int lastLayerNeuronsCount = mLayers.at(lastLayerIdx)->neuronsCount();
+	const int lastLayerIdx = mLayers.size() - 1;
+	const int lastLayerNeuronsCount = mLayers.at(lastLayerIdx)->neuronsCount();
 
-    QVector<nnreal> output = classifyVec(input);
+	QVector<nnreal> output = classifyVec(input);
 
-    QVector<nnreal> lastLayerError;
-    lastLayerError.resize(lastLayerNeuronsCount);
+	QVector<nnreal> lastLayerError;
+	lastLayerError.resize(lastLayerNeuronsCount);
 
-    NetworkLayer *prevLayer = mLayers.at(lastLayerIdx - 1);
+	NetworkLayer *prevLayer = mLayers.at(lastLayerIdx - 1);
 
-    QVector<nnreal> previousLayerError(prevLayer->neuronsCount());
+	QVector<nnreal> previousLayerError(prevLayer->neuronsCount());
 
-    nnreal averageError = 0.0;
+	nnreal averageError = 0.0;
 
-    for (int i = 0; i < lastLayerNeuronsCount; i++) {
-        Neuron *neuron = mLayers.at(lastLayerIdx)->neuron(i);
+	for (int i = 0; i < lastLayerNeuronsCount; i++) {
+		Neuron *neuron = mLayers.at(lastLayerIdx)->neuron(i);
 
-        const nnreal errorTemp = targetOutput.at(i) - output.at(i);
-        neuron->updateDelta(errorTemp);
+		const nnreal errorTemp = targetOutput.at(i) - output.at(i);
+		neuron->updateDelta(errorTemp);
 
-        averageError += errorTemp * errorTemp;
+		averageError += errorTemp * errorTemp;
 
-        const int connCount = neuron->connectionsCount();
-        const nnreal delta = neuron->delta();
-        for (int j = 0; j < connCount; j++) {
-            Connection *conn = neuron->connection(j);
-            const nnreal weight = conn->weight();
-            const int endNeuron = conn->neuronIndex();
-            previousLayerError[endNeuron] += delta * weight;
+		const int connCount = neuron->connectionsCount();
+		const nnreal delta = neuron->delta();
+		for (int j = 0; j < connCount; j++) {
+			Connection *conn = neuron->connection(j);
+			const nnreal weight = conn->weight();
+			const int endNeuron = conn->neuronIndex();
+			previousLayerError[endNeuron] += delta * weight;
 
-            const nnreal momentumCoef = momentum * conn->weightDelta();
-            const nnreal prevNeuronOut = prevLayer->neuron(endNeuron)->output();
-            const nnreal lrCoef = prevNeuronOut * delta * learningRate;
-            const nnreal weightDelta = momentumCoef + lrCoef;
-            conn->setWeightDelta(weightDelta);
-            conn->setWeight(weight + weightDelta);
-        }
+			const nnreal momentumCoef = momentum * conn->weightDelta();
+			const nnreal prevNeuronOut = prevLayer->neuron(endNeuron)->output();
+			const nnreal lrCoef = prevNeuronOut * delta * learningRate;
+			const nnreal weightDelta = momentumCoef + lrCoef;
+			conn->setWeightDelta(weightDelta);
+			conn->setWeight(weight + weightDelta);
+		}
 
-        nnreal momentumCoef = 0;
+		nnreal momentumCoef = 0;
 #ifdef TEACH_BIAS_MOMENTUM
-        momentumCoef = momentum * neuron->biasDelta();
+		momentumCoef = momentum * neuron->biasDelta();
 #endif
-        const nnreal lrCoef = learningRate * delta;
-        const nnreal weightDelta = momentumCoef + lrCoef;
-        neuron->setBiasDelta(weightDelta);
-        neuron->setBias(neuron->bias() + weightDelta);
-    }
+		const nnreal lrCoef = learningRate * delta;
+		const nnreal weightDelta = momentumCoef + lrCoef;
+		neuron->setBiasDelta(weightDelta);
+		neuron->setBias(neuron->bias() + weightDelta);
+	}
 
-    for (int i = lastLayerIdx - 1; i > 0; i--) {
-        previousLayerError = mLayers.at(i)->backprop(previousLayerError,
-                                                     learningRate, momentum);
-    }
+	for (int i = lastLayerIdx - 1; i > 0; i--) {
+		previousLayerError = mLayers.at(i)->backprop(previousLayerError,
+													 learningRate, momentum);
+	}
 
    // qDebug()<<output<<targetOutput<<averageError;
    // qDebug()<<fabs(averageError / (float)lastLayerNeuronsCount);
-    return fabs(averageError) / (float)lastLayerNeuronsCount;
+	return fabs(averageError) / (float)lastLayerNeuronsCount;
 }
 
 
 void NeuralNetwork::addLayer(const int &numNeurons,
-                             const Neuron::ActivationType &activationFunctionType)
+							 const Neuron::ActivationType &activationFunctionType)
 {
-    NetworkLayer *newLayer;
-    if (!mLayers.isEmpty()) {
-        newLayer = new NetworkLayer(mLayers.last(), activationFunctionType);
-    } else {
-        newLayer = new NetworkLayer(NULL, activationFunctionType);
-    }
+	NetworkLayer *newLayer;
+	if (!mLayers.isEmpty()) {
+		newLayer = new NetworkLayer(mLayers.last(), activationFunctionType);
+	} else {
+		newLayer = new NetworkLayer(NULL, activationFunctionType);
+	}
 
-    mLayers.append(newLayer);
-    newLayer->addNeurons(numNeurons);
+	mLayers.append(newLayer);
+	newLayer->addNeurons(numNeurons);
 }
 
 quint8 NeuralNetwork::classify(const QVector<nnreal> &input)
 {
-    QVector<nnreal> output = classifyVec(input);
+	QVector<nnreal> output = classifyVec(input);
 
-    return outputVectorToLabel(output);
+	return outputVectorToLabel(output);
 }
 
 quint8 NeuralNetwork::outputVectorToLabel(const QVector<nnreal> &output) const
 {
-    int maxIndex = 0;
-    nnreal max = -std::numeric_limits<nnreal>::infinity();
+	int maxIndex = 0;
+	nnreal max = -std::numeric_limits<nnreal>::infinity();
 
-    for (int i = 0; i < output.size(); i++) {
-        if (output.at(i) > max) {
-            max = output.at(i);
-            maxIndex = i;
-        }
-    }
-    quint8 result;
-    switch (maxIndex) {
-        default:
-        case 0:
-            result = 32;
-            break;
-        case 1:
-            result = 96;
-            break;
-        case 2:
-            result = 160;
-            break;
-        case 3:
-            result = 224;
-            break;
-    }
-    return result;
+	for (int i = 0; i < output.size(); i++) {
+		if (output.at(i) > max) {
+			max = output.at(i);
+			maxIndex = i;
+		}
+	}
+	quint8 result;
+	switch (maxIndex) {
+		default:
+		case 0:
+			result = 32;
+			break;
+		case 1:
+			result = 96;
+			break;
+		case 2:
+			result = 160;
+			break;
+		case 3:
+			result = 224;
+			break;
+	}
+	return result;
 }
 
 QVector<float> NeuralNetwork::train(const QVector<QVector<nnreal> > &train,
-                          const QVector<QVector<nnreal> > &exOutput,
-                          const int &maxEpochs, const nnreal &learningRate,
-                          const nnreal &momentum, const nnreal &desiredError,
-                          QDataStream &nnout)
+						  const QVector<QVector<nnreal> > &exOutput,
+						  const int &maxEpochs, const nnreal &learningRate,
+						  const nnreal &momentum, const nnreal &desiredError,
+						  QDataStream &nnout)
 {
-    QTextStream out(stdout);
-    nnreal epochError = std::numeric_limits<nnreal>::infinity();
-    int epochNum = 0;
-    double lr = learningRate;
-    QList<int> indicesBase;
-    QVector<float> result;
+	QVector<float> result;
+//	QFile trainInspectFile("train.log");
+//	QTextStream trainLogStream(&trainInspectFile);
+//	if (!trainInspectFile.open(QIODevice::WriteOnly)) {
+//		qDebug() << "failed to open train.log file";
+//		return result;
+//	}
+	QTextStream out(stdout);
+	nnreal epochError = std::numeric_limits<nnreal>::infinity();
+	int epochNum = 0;
+	double lr = learningRate;
+	QList<int> indicesBase;
+	//qDebug() << "training with:" << train;
 #ifdef HAS_VECTOR_RESERVE
-    indicesBase.reserve(train.size());
-    result.reserve(maxEpochs);
+	indicesBase.reserve(train.size());
+	result.reserve(maxEpochs);
 #endif
-    for (int i = 0; i < train.size(); i++) {
-        indicesBase.append(i);
-    }
+	for (int i = 0; i < train.size(); i++) {
+		indicesBase.append(i);
+	}
 #ifdef HAS_ELAPSED_TIMER
-    QElapsedTimer epochTimer;
+	QElapsedTimer epochTimer;
 #else
-    QTime epochTimer;
+	QTime epochTimer;
 #endif
-    epochTimer.start();
-    float best = std::numeric_limits<nnreal>::infinity();
-    while (epochError > desiredError && epochNum < maxEpochs) {
+	epochTimer.start();
+	float best = std::numeric_limits<nnreal>::infinity();
+	while (epochError > desiredError && epochNum < maxEpochs) {
 
-        epochError = 0;
+		epochError = 0;
 
-        QList<int> indices = indicesBase;
+		QList<int> indices = indicesBase;
 
-        int sampleNum = 0;
-        while (!indices.empty()) {
-            const int index = qrand() % indices.size();
-            const int i = indices.takeAt(index);
-            const QVector<nnreal> input = train.at(i);
-            const QVector<nnreal> output = exOutput.at(i);
-            sampleNum++;
+		int sampleNum = 0;
+		while (!indices.empty()) {
+			const int index = qrand() % indices.size();
+			const int i = indices.takeAt(index);
+			const QVector<nnreal> input = train.at(i);
+			const QVector<nnreal> output = exOutput.at(i);
+			sampleNum++;
 
-            //qDebug()<<output;
+			//qDebug()<<output;
 
-            epochError += backprop(input, output, lr, momentum);
+			epochError += backprop(input, output, lr, momentum);
 
-            //out<<"Epoch: "<<epochNum<<" progress: "<<(int)(100*(float)sampleNum/trainingSetSize)<<"%";
-            //if(!indices.empty())out<<"\r";
-        }
+			//out<<"Epoch: "<<epochNum<<" progress: "<<(int)(100*(float)sampleNum/trainingSetSize)<<"%";
+			//if(!indices.empty())out<<"\r";
+		}
 
-        epochError /= train.size();
-        bool saved = false;
-        if (epochError < best) {
-            best = epochError;
-            nnout << *this;
-            saved = true;
-        } else if (epochError > best * 3 / 2) {
-            epochNum = maxEpochs;
-        }
-        result << epochError;
+		epochError /= train.size();
+		bool saved = false;
+		if (epochError < best) {
+			best = epochError;
+			nnout << *this;
+			saved = true;
+//			int correct = 0;
+//			for (int i = 0; i < train.size(); i++) {
+//				const QVector<nnreal> was = classifyVec(train.at(i));
+//				const QVector<nnreal> expected = exOutput.at(i);
+//				const int wc = outputVectorToLabel(was);
+//				const int ec = outputVectorToLabel(expected);
+//				if (wc == ec) {
+//					correct++;
+//				}
+//				qDebug() << i << "was" << was << "(" << wc << "), expected:" << expected << "(" << ec << ")";
+//			}
+//			qDebug() << "correct:" << nnreal(correct) / nnreal(train.size());
+//			trainLogStream << "epoch " << epochNum << endl;
+//			trainLogStream << *this << endl << endl;
+		} else if (epochError > best * 3.0 / 0.1) {
+			epochNum = maxEpochs;
+		}
+		result << epochError;
 
-        lr *= 0.999;
-        const int msecs = epochTimer.restart();
-        out << "Epoch: " << epochNum << " error: " << epochError << ", "
-            << msecs << "msecs, saved: " << saved << endl;
-        epochNum++;
-    }
-    return result;
+		lr *= 0.999;
+		const int msecs = epochTimer.restart();
+		out << "Epoch: " << epochNum << " error: " << epochError << ", "
+			<< msecs << "msecs, saved: " << saved << endl;
+		epochNum++;
+	}
+//	trainInspectFile.close();
+	return result;
 }
 
 QDataStream &operator<<(QDataStream &stream, const NeuralNetwork &nn)
